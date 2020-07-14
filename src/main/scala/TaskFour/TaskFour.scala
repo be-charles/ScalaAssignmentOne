8f6f658f8e8ad3ee52bf32d74a935c2c2aeae856
Brandon Charles <brandoncharles.freelance@gmail.com>
2020-07-14 13:18:04 +0200

    Task Four Changed the way course id is accessed (no longer accessed as parameterless function)

--- src/main/scala/TaskFour/TaskFour.scala
+++ src/main/scala/TaskFour/TaskFour.scala
@@ -8,7 +8,7 @@ object TaskFour {
   def main(args: Array[String]): Unit = {
     val courses = Array(Course("ICE362S Elective, FT", 40000), Course("ICE262S Elective, FT", 35000), Course("ICE152S Elective, FT", 30000))
 
-    for(course <- courses){ println(course.courseId) }
+    for(course <- courses){ println(course.courseId()) }
   }
 }
 
