0020ae05dd9d262c115f793c6cfa0553c24d6c45
Brandon Charles <brandoncharles.freelance@gmail.com>
2020-07-14 11:15:36 +0200

    Task One Completed

---
+++ src/main/scala/TaskOne.scala
@@ -0,0 +1,16 @@
+package main.scala
+
+import scala.collection.mutable.ListBuffer
+
+object TaskOne {
+
+  def main(args: Array[String]): Unit = {
+    val numbers:ListBuffer[Int] = getNumbers()
+    print(numbers)
+  }
+  def getNumbers(): ListBuffer[Int] = {
+    val lb = new ListBuffer[Int]
+    for(i <- 50 to 100){ lb.addOne(i) }
+    lb
+  }
+}
